a24f6581cf9ec051b4ba6079d8c9ded6559b7c70
Andreas Martin <andreas.martin@tiket.com>
2023-05-11 02:55:29 +0700

    feat!: upgrade major version of a

---
+++ a/v2/a.go
@@ -0,0 +1,9 @@
+package a
+
+import (
+	v1 "github.com/AVM-tiket/AVM-GO-COMMON/a"
+)
+
+func Echo(s string) string {
+	return v1.Echo(s)
+}
---
+++ a/v2/go.mod
@@ -0,0 +1,5 @@
+module github.com/AVM-tiket/AVM-GO-COMMON/a/v2
+
+go 1.20
+
+require github.com/AVM-tiket/AVM-GO-COMMON/a v1.1.0
